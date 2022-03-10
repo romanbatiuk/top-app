69b47b67f9444614e5f6ea92f01ce36acde0cd3f
Roman <roman.batiuk87@gmail.com>
2022-03-10 19:45:10 +0200

    layout grid & footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
 import { FooterProps } from './Footer.props';
+import { format } from 'date-fns';
 import styles from './Footer.module.css';
+import cn from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} All rights reserved</div>
+			<a href="#" target="_blank">
+				Terms of use
+			</a>
+			<a href="#" target="_blank">
+				Privacy policy
+			</a>
+		</footer>
+	);
 };
