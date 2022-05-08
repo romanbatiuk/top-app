a1d203de16b22c8d6d8b4283fede831331749499
romanbatiuk <roman.batiuk87@gmail.com>
2022-05-08 17:54:22 +0300

    ReviewForm validate use react-hook-form

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -2,69 +2,75 @@ import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
 import StarIcon from './star.svg';
 import cn from 'classnames';
-import { KeyboardEvent, useEffect, useState } from 'react';
+import { ForwardedRef, forwardRef, KeyboardEvent, useEffect, useState } from 'react';
 
-export const Rating = ({
-	isEditable = false,
-	rating,
-	setRating,
-	...props
-}: RatingProps): JSX.Element => {
-	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+export const Rating = forwardRef(
+	(
+		{ isEditable = false, rating, setRating, error, ...props }: RatingProps,
+		ref: ForwardedRef<HTMLDivElement>
+	): JSX.Element => {
+		const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
-	useEffect(() => {
-		constructRating(rating);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [rating]);
+		useEffect(() => {
+			constructRating(rating);
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, [rating]);
 
-	const constructRating = (currentRating: number) => {
-		const updatedArray = ratingArray.map((r: JSX.Element, index: number) => {
-			return (
-				<span
-					key={index}
-					className={cn(styles.star, {
-						[styles.filled]: index < currentRating,
-						[styles.editable]: isEditable
-					})}
-					onMouseEnter={() => changeDisplay(index + 1)}
-					onMouseLeave={() => changeDisplay(rating)}
-					onClick={() => onclick(index + 1)}>
-					<StarIcon
-						tabIndex={isEditable ? 0 : -1}
-						onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(index + 1, e)}
-					/>
-				</span>
-			);
-		});
-		setRatingArray(updatedArray);
-	};
+		const constructRating = (currentRating: number) => {
+			const updatedArray = ratingArray.map((r: JSX.Element, index: number) => {
+				return (
+					<span
+						key={index}
+						className={cn(styles.star, {
+							[styles.filled]: index < currentRating,
+							[styles.editable]: isEditable
+						})}
+						onMouseEnter={() => changeDisplay(index + 1)}
+						onMouseLeave={() => changeDisplay(rating)}
+						onClick={() => onclick(index + 1)}>
+						<StarIcon
+							tabIndex={isEditable ? 0 : -1}
+							onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(index + 1, e)}
+						/>
+					</span>
+				);
+			});
+			setRatingArray(updatedArray);
+		};
 
-	const changeDisplay = (index: number) => {
-		if (!isEditable) {
-			return;
-		}
-		constructRating(index);
-	};
+		const changeDisplay = (index: number) => {
+			if (!isEditable) {
+				return;
+			}
+			constructRating(index);
+		};
 
-	const onclick = (index: number) => {
-		if (!isEditable || !setRating) {
-			return;
-		}
-		setRating(index);
-	};
+		const onclick = (index: number) => {
+			if (!isEditable || !setRating) {
+				return;
+			}
+			setRating(index);
+		};
 
-	const handleSpace = (index: number, e: KeyboardEvent<SVGAElement>) => {
-		if (e.code != 'Space' || !setRating) {
-			return;
-		}
-		setRating(index);
-	};
+		const handleSpace = (index: number, e: KeyboardEvent<SVGAElement>) => {
+			if (e.code != 'Space' || !setRating) {
+				return;
+			}
+			setRating(index);
+		};
 
-	return (
-		<div {...props}>
-			{ratingArray.map((rating: JSX.Element, index: number) => (
-				<span key={index}>{rating}</span>
-			))}
-		</div>
-	);
-};
+		return (
+			<div
+				{...props}
+				ref={ref}
+				className={cn(styles.ratingWrapper, {
+					[styles.error]: error
+				})}>
+				{ratingArray.map((rating: JSX.Element, index: number) => (
+					<span key={index}>{rating}</span>
+				))}
+				{error && <span className={styles.errorText}>{error.message}</span>}
+			</div>
+		);
+	}
+);
--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -6,32 +6,75 @@ import { Input } from '../Input/Input';
 import { Rating } from '../Rating/Rating';
 import { Textarea } from '../Textarea/Textarea';
 import { Button } from '../Button/Button';
+import { useForm, Controller } from 'react-hook-form';
+import { IReviewForm } from './ReviewForm.interface';
 
-export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => (
-	<>
-		<div className={cn(styles.reviewForm, className)} {...props}>
-			<Input placeholder="Имя" />
-			<Input placeholder="Заголовок отзыва" />
+export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<IReviewForm>();
 
-			<div className={styles.rating}>
-				<div>Оценка курса:</div>
-				<Rating rating={0} />
+	const onSubmit = (data: IReviewForm) => {
+		console.log(data);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className={cn(styles.reviewForm, className)} {...props}>
+				<Input
+					{...register('name', { required: { value: true, message: 'Заполните имя' } })}
+					placeholder="Имя"
+					error={errors.name}
+				/>
+				<Input
+					{...register('title', { required: { value: true, message: 'Заполните заголовок' } })}
+					placeholder="Заголовок отзыва"
+					error={errors.title}
+				/>
+
+				<div className={styles.rating}>
+					<div>Оценка курса:</div>
+					<Controller
+						control={control}
+						name="rating"
+						rules={{ required: { value: true, message: 'Укажите рейтинг' } }}
+						render={({ field }) => (
+							<Rating
+								isEditable
+								rating={field.value}
+								ref={field.ref}
+								setRating={field.onChange}
+								error={errors.rating}
+							/>
+						)}
+					/>
+				</div>
+
+				<Textarea
+					{...register('description', {
+						required: { value: true, message: 'Заполните текст отзыва' }
+					})}
+					className={styles.textArea}
+					placeholder="Текст отзыва"
+					error={errors.description}
+				/>
+
+				<div className={styles.submit}>
+					<Button appearance="primary">Отправить</Button>
+					<span className={styles.submitInfo}>
+						* Перед публикацией отзыва о курсе, он пройдет предварительную модерацию и проверку
+					</span>
+				</div>
 			</div>
 
-			<Textarea className={styles.textArea} placeholder="Текст отзыва" />
-
-			<div className={styles.submit}>
-				<Button appearance="primary">Отправить</Button>
-				<span className={styles.submitInfo}>
-					* Перед публикацией отзыва о курсе, он пройдет предварительную модерацию и проверку
-				</span>
+			<div className={styles.success}>
+				<div className={styles.successTitle}>Ваш отзыв отправлен</div>
+				<div>Спасибо за Ваш отзыв, он будет опубликован после проверки модератором!</div>
+				<CloseIcon className={styles.close} />
 			</div>
-		</div>
-
-		<div className={styles.success}>
-			<div className={styles.successTitle}>Ваш отзыв отправлен</div>
-			<div>Спасибо за Ваш отзыв, он будет опубликован после проверки модератором!</div>
-			<CloseIcon className={styles.close} />
-		</div>
-	</>
-);
+		</form>
+	);
+};
